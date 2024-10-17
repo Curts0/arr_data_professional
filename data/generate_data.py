5b11b4383b09e3a1c7b7a146ba80c41357a53a03
Curtis Stallings <curtisrstallings@gmail.com>
2024-10-17 11:47:38 -0500

    before major changes

---
+++ data/generate_data.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+products = [
+    
+]
+
+'''
+customer
+product
+product_family
+contract
+
+contract
+id, item_sku,
+header_start_date, header_end_date,
+line_start_date, line_end_date,
+amount,
+customer
+'''
